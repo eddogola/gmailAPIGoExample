e401aca2a94030755e107dd383e8c7d2d77239c3
Ogola Eddy <eddogola01@gmail.com>
2021-02-11 18:14:44 +0300

    removed error check func(it's general)

--- main.go
+++ main.go
@@ -14,12 +14,6 @@ import (
 	"google.golang.org/api/gmail/v1"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Printf("Error: %v", err)
-	}
-}
-
 func createClient(config *oauth2.Config) *http.Client {
 	tokenFile := "token.json"
 	token, err := getTokenFromFile(tokenFile)
@@ -32,16 +26,12 @@ func createClient(config *oauth2.Config) *http.Client {
 	return config.Client(context.Background(), token)
 }
 
-func getConfig() *oauth2.Config {
+func getConfig() (*oauth2.Config, error) {
 	filename := "credentials.json"
 	credentials, err := ioutil.ReadFile(filename)
-
-	check(err)
-
 	config, err := google.ConfigFromJSON(credentials, gmail.GmailReadonlyScope)
-	check(err)
 
-	return config
+	return config, err
 }
 
 func getTokenFromFile(filename string) (*oauth2.Token, error) {
@@ -64,12 +54,16 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 
 	// grabs the authorization code you paste into the terminal
 	var code string
-	_, err := fmt.Scan(&code)
-	check(err)
+
+	if _, err := fmt.Scan(&code); err != nil  {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
 
 	// exchange the code for an access token
 	token, err := config.Exchange(context.TODO(), code)
-	check(err)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
 
 	return token
 }
@@ -79,14 +73,19 @@ func saveTokenToFile(path string, token *oauth2.Token) {
 	fmt.Printf("saving credential file to: %v", path)
 
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	check(err)
+	if err != nil {
+		log.Fatalf("unable to cache oauth token: %v", err)
+	}
 
 	defer file.Close()
 	json.NewEncoder(file).Encode(token)
 }
 
 func main() {
-	config := getConfig()
+	config, err := getConfig()
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
 
 	client := createClient(config)
 
